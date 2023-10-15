22f591e8b811a4d27dc42e6043405a48d1e55561
AANYA <aanya20419@iiitd.ac.in>
2023-10-15 22:52:22 +0530

    delays added
    added
    1. delays.txt delays file to read delays from
    2. updated router class to include three types of delays, calculate the clock period and clock frequency
    3. Updated mesh to define router with delays and subsequently calculate clock values

--- Mesh.py
+++ Mesh.py
@@ -1,20 +1,37 @@
 from Router import Router
 
+
 class Mesh:
     def __init__(self):
         self.Mesh2D = []
-        for i in range(9):  # Create 9 routers
-            self.Mesh2D.append(Router(str(i)))
-            
+
+        with open('delays.txt', 'r') as delayFile:
+            delayData = delayFile.readlines()
+
+        # for i in range(9):  # Create 9 routers
+        i1 = 0
+        while i1 < 9:  # Create 9 routers
+
+            RouterDelay = delayData[i1].strip().split()
+            sa_delay = float(RouterDelay[0])
+            xbar_delay = float(RouterDelay[1])
+            buffer_delay = float(RouterDelay[2])
+            self.Mesh2D.append(
+                Router(str(i), sa_delay, xbar_delay, buffer_delay))
+            Router.CalculateClockValues(self.Mesh2D[i1])
+
             for i in range(3):
                 for j in range(3):
                     index = i * 3 + j
                     if j > 0:  # Connect to the left router
-                        Router.LeftConnect(self.Mesh2D[index - 1],self.Mesh2D[index])
-                    
-                    if i > 0:  # Connect to the router above
-                        Router.UpConnect(self.Mesh2D[index - 3],self.Mesh2D[index])
+                        Router.LeftConnect(
+                            self.Mesh2D[index - 1], self.Mesh2D[index])
 
-    def injectPacket(self,packet):
+                    if i > 0:  # Connect to the router above
+                        Router.UpConnect(
+                            self.Mesh2D[index - 3], self.Mesh2D[index])
+            i1 = i1+1
+
+    def injectPacket(self, packet):
         source = packet.getSource()
-        self.Mesh2D[source].RecievePacket("Local",packet)
+        self.Mesh2D[source].RecievePacket("Local", packet)
