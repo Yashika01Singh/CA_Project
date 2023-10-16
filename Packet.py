582f4e5cac3448e5aa601ffc324d7696d7cb9b69
YashikaSingh01 <yashika20161@iiitd.ac.in>
2023-10-16 21:53:52 +0530

    Packet_init

--- Packet.py
+++ Packet.py
@@ -1,10 +1,11 @@
 class Packet:
      def __init__(self,input):
           #create the required flit
-          self.source = ""
-          self.destination = ""
-          self.clockcycle = ""
-          self.flitData = ""
+          input = input.split(" ")
+          self.source = input[1]
+          self.destination = input[2]
+          self.clockcycle = input[0]
+          self.flitData = input[3]
      
      """def getflits(self):
           #return flits in a list like [head,body,tail]
